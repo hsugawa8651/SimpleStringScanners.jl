fa02b64ea20359d78dda5d76740020f1aca4de16
Documenter.jl <documenter@juliadocs.github.io>
2024-03-16 06:21:17 +0000

    build based on db504a7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleStringScanners","category":"page"},{"location":"#SimpleStringScanners","page":"Home","title":"SimpleStringScanners","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimpleStringScanners.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleStringScanners]","category":"page"}]
+}
